3a454801f22e8a8645875a127f32d4430c90a4d2
Szymon Pinisz <89405330+SzymonPinisz@users.noreply.github.com>
2021-10-05 20:00:27 +0200

    Entity addition

---
+++ ZadKonc/src/Entity/Product.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Entity\ColorPropertyInterface;
+use App\Entity\ProductInterface;
+use Sylius\Component\Core\Model\Product as BaseProduct;
+use Sylius\Component\Product\Model\ProductTranslationInterface;
+
+class Product extends BaseProduct implements ProductInterface{
+
+    /** @var ColorPropertyInterface */
+    private $color;
+
+    public function getColor(): ?ColorPropertyInterface {
+        return $this -> color;
+    }public function setColor(ColorPropertyInterface $color): void {
+        $this -> color = $color;
+    }
+
+    //This function is for twig purposes
+    public function getColorProperty(): String {
+        if($this -> color){
+            return $this -> color -> getColor();
+        }else{
+            return "";
+        }
+    }
+}
